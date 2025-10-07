dec26070120b16001a4c754fde50c214d3fafdd2
codewithrabby <iammdgolamrabby@gmail.com>
2025-10-08 01:49:38 +0600

    nav section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,52 @@
 
 import { Outlet } from 'react-router'
 import './App.css'
+import logo from './assets/logo.png'
+import { FaGithub } from "react-icons/fa";
 
 function App() {
 
 
   return (
     <>
-      <h1>Header</h1>
+      <nav className="flex justify-between items-center max-w-7xl mx-auto h-[80px] px-6">
+  <div className="flex items-center gap-2">
+    <img src={logo} alt="logo" className="w-10 h-10" />
+    <h3 className="text-2xl font-semibold bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-transparent bg-clip-text">
+  App Nest
+</h3>
+
+  </div>
+
+  <div className="flex items-center gap-8 text-lg font-medium">
+    <a href="#">Home</a>
+    <a href="#">Apps</a>
+    <a href="#">Installation</a>
+  </div>
+
+  <div className="flex items-center gap-3">
+    <button className="flex items-center gap-2 text-white px-4 py-2 rounded-lg 
+bg-gradient-to-r from-[#632EE3] to-[#9F62F2] hover:opacity-90 transition">
+  <FaGithub className="text-2xl" />
+  Contribute
+</button>
+
+  </div>
+</nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       <Outlet></Outlet>
       <h1>Footer</h1>
     </>
