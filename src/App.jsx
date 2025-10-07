fe88f566e054ae236258708207fc097337f7dd3f
codewithrabby <iammdgolamrabby@gmail.com>
2025-10-08 02:59:36 +0600

    added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -25,24 +25,13 @@ function App() {
   </div>
 
   <div className="flex items-center gap-3">
-    <button className="flex items-center gap-2 text-white px-4 py-2 rounded-lg 
-bg-gradient-to-r from-[#632EE3] to-[#9F62F2] hover:opacity-90 transition">
+    <button className="flex items-center gap-2 text-white px-4 py-2 rounded-lg bg-gradient-to-r from-[#632EE3] to-[#9F62F2] hover:opacity-90 transition">
   <FaGithub className="text-2xl" />
   Contribute
 </button>
 
   </div>
 </nav>
-
-
-
-
-
-
-
-
-
-
 
 
 
